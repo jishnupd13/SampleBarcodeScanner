469a808565bac52bb5cd54a704c42b81f7819246
jishnu <jishnu.webandcrafts@gmail.com>
2020-12-08 13:45:19 +0530

    messages

--- app/src/main/java/com/recycler/mvvmbasepackapplication/ui/MainActivity.kt
+++ app/src/main/java/com/recycler/mvvmbasepackapplication/ui/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.recycler.mvvmbasepackapplication.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.recycler.mvvmbasepackapplication.R
+import com.recycler.mvvmbasepackapplication.data.models.User
 import com.recycler.mvvmbasepackapplication.databinding.ActivityMainBinding
+import com.recycler.mvvmbasepackapplication.ui.adapters.MainAdapter
 import com.recycler.mvvmbasepackapplication.utils.Status
-import com.recycler.mvvmbasepackapplication.utils.ToastUtils
 import com.recycler.mvvmbasepackapplication.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
 
 /**
  * created By Jishnu P Dileep
@@ -21,28 +25,41 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val mainViewModel : MainViewModel by viewModels()
+    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
-
-
+    private var userList= arrayListOf<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         mainViewModel.users.observe(this, Observer {
             when (it.status) {
                 Status.SUCCESS -> {
+                    binding.userRecyclerView.visibility=View.VISIBLE
+                    binding.appLoader.visibility=View.GONE
+
+                    if(it.data!=null){
+
+                        userList.addAll(it.data)
+                        val mainAdapter=MainAdapter(userList)
+                        val llm = LinearLayoutManager(this)
+                        llm.orientation = LinearLayoutManager.VERTICAL
+                        binding.userRecyclerView.layoutManager = llm
+                        binding.userRecyclerView.adapter=mainAdapter
+                    }
 
                 }
-                Status.LOADING -> {
 
-                }
                 Status.ERROR -> {
+                    binding.userRecyclerView.visibility=View.VISIBLE
+                    binding.appLoader.visibility=View.GONE
+                }
 
+                Status.LOADING -> {
+                    binding.userRecyclerView.visibility=View.GONE
+                    binding.appLoader.visibility=View.VISIBLE
                 }
             }
         })
--- app/src/main/java/com/recycler/mvvmbasepackapplication/ui/adapters/MainAdapter.kt
+++ app/src/main/java/com/recycler/mvvmbasepackapplication/ui/adapters/MainAdapter.kt
@@ -1,10 +1,39 @@
 package com.recycler.mvvmbasepackapplication.ui.adapters
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.recycler.mvvmbasepackapplication.R
+import com.recycler.mvvmbasepackapplication.data.models.User
+import com.recycler.mvvmbasepackapplication.databinding.ItemUserNameBinding
+
 /**
  * created By Jishnu P Dileep
  * 01-12-2020
  * */
 
 
-class MainAdapter{
+class MainAdapter(private val userList: List<User>) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val binding: ItemUserNameBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_user_name, parent, false)
+        return MainViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+       holder.binding.item= userList[position]
+    }
+
+    class MainViewHolder(itemUserNameBinding: ItemUserNameBinding) :
+        RecyclerView.ViewHolder(itemUserNameBinding.root) {
+        val binding = itemUserNameBinding
+    }
+
+
 }
